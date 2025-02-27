24e37ad7ce70634b22295672ef665b7075fe3a8f
Aditya2344s <singaditya934@gmail.com>
2025-02-27 15:24:34 +0530

    rectangle thickness& sharness adjusted

--- face_recognize.py
+++ face_recognize.py
@@ -5,14 +5,23 @@ import os
 
 mp_face_detection = mp.solutions.face_detection
 mp_drawing = mp.solutions.drawing_utils
-
+import numpy as np
 video = cv2.VideoCapture(0)
+
+def sharpen_image(image, strength=1.0):
+    kernel = np.array([[0, -1, 0],
+                       [-1,  5 + strength, -1],
+                       [0, -1, 0]]) / (1 + strength)  # Normalize to prevent excessive sharpening
+    sharp_image = cv2.filter2D(image, -1, kernel)
+    return sharp_image
 
 with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5) as face_detection:
     while True:
         ret, frame = video.read()
         if not ret:
             break
+
+        frame = sharpen_image(frame,strength=0.5)
 
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = face_detection.process(rgb_frame)
@@ -31,3 +40,4 @@ with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence
 
 video.release()
 cv2.destroyAllWindows()
+ 
