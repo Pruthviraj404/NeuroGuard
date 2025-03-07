15806f3c5795c50964433d09ef7327dc5044dc20
Aditya2344s <singaditya934@gmail.com>
2025-03-07 15:17:32 +0530

    added_web_pages

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app= Flask(__name__)
 
 @app.route('/')
 def home():
-    return "hello world"
+    return render_template("index.html")
+
+@app.route('/admin')
+def admin():
+    return render_template("admin_dashboard.html")
 
 
 if __name__=='__main__':
