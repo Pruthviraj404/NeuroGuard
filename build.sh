fd7b64d6be64631eeabcbea04f3653223fe01e5e
Pruthviraj Patil <pruthvirajp548@gmail.com>
2025-03-11 20:27:55 +0530

    updated_build

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
+# Install system dependencies
+apt-get update && apt-get install -y build-essential python3-dev python3-pip
+
+# Upgrade pip
+pip install --upgrade pip setuptools wheel
+
+# Install Python dependencies
 pip install -r requirements.txt
-apt-get install build-essential
