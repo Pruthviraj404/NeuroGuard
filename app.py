ffff0661062714b756b2ae2e767e3360cea284c0
Pruthviraj Patil <pruthvirajp548@gmail.com>
2025-03-19 21:18:01 +0530

    updates

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def save_image():
     return jsonify({"message": "Images saved successfully", "files": saved_images})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
